e2d806edbd5b963bbed2f0f8c241b880c4dcdb92
Vincent Zvikaramba <zvikovincent@gmail.com>
2022-02-14 02:54:02 -0500

    Record cache
    Change-Id: I4048db384878401051a8e02f3f6adc4622794ded

--- update_cf_record.py
+++ update_cf_record.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
 import json
 import sys
 import requests
@@ -31,6 +32,57 @@ class _batch:
         self.token = token
         self.records = records
         self.force = force
+
+class RecordCache:
+    class CacheObject:
+        def __init__(self, data):
+            '''
+            '''
+            self.data = data
+            self.timestamp = datetime.datetime.now()
+
+    CACHE_LIFETIME = 120
+    CACHE_FILE = "/tmp/.ucr.cache.json"
+
+    def __init__(self, cache_lifetime = CACHE_LIFETIME, cache_file = CACHE_FILE):
+        '''
+        '''
+        self.cache = {}
+        self.cache_lifetime = cache_lifetime
+        self.cache_file = cache_file
+
+    def load(self):
+        '''
+        '''
+        cache_map = read_json_file(self.cache_file)
+        if cache_map:
+            self.cache = cache_map
+
+    def save(self):
+        '''
+        '''
+        write_json_file(self.cache, self.cache_file)
+
+    def get(self, key: 'object', default_value: 'object' = None) -> 'object':
+        '''
+        '''
+        if key not in self.cache.keys():
+            return default_value
+
+        value = self.cache[key]
+        delta = datetime.datetime.now() - value.timestamp
+        if delta.total_seconds() <= self.cache_lifetime:
+            return value.data
+
+        self.cache.pop(key)
+        return default_value
+
+    def put(self, key: 'object', value: 'object') -> 'None':
+        '''
+        '''
+        self.cache[key] = RecordCache.CacheObject(value)
+
+CACHE = RecordCache()
 
 def get_public_address() -> 'tuple[str,str]':
     '''
@@ -122,11 +174,17 @@ def get_zones(cf: 'CF.CloudFlare', zone_name: 'str' = None) -> 'list':
     ''' Get the zone identifiers
     '''
     try:
-        params = {}
-        if zone_name:
-            params['name'] = zone_name
+        zones = CACHE.get('zones')
+        if zones:
+            return zones
 
-        zones = cf.zones.get(params=params)
+        zones = cf.zones.get(params = {})
+        CACHE.put('zones', zones)
+
+        for zone in zones:
+            if zone['name'] == zone_name:
+                return zone
+
     except CF.exceptions.CloudFlareAPIError as e:
         sys.exit('/zones %d %s - api call failed' % (e, e))
     except Exception as e:
@@ -134,22 +192,28 @@ def get_zones(cf: 'CF.CloudFlare', zone_name: 'str' = None) -> 'list':
 
     return zones
 
-def get_zone_name(name: 'str') -> 'str':
+def get_zone_map(cf: 'CF.CloudFlare', zone_name: 'str' = None) -> 'dict':
+    ''' Get the zone identifiers
+    '''
+    ret = {}
+    for zone in get_zones(cf, zone_name):
+        ret[zone['name']] = zone
+
+    return ret
+
+def get_zone_name(cf: 'CF.CloudFlare', name: 'str') -> 'str':
     '''
     Get zone name from fully-qualified name
     '''
+    zone_map = get_zone_map(cf)
     split = name.split('.')
-    if len(split) < 2:
-        print("Invalid name {} specified".format(name), file=sys.stderr)
-        return ''
 
-    join_index = -2
+    for i in range(0, len(name)):
+        current = ".".join(split[i:])
+        if current in zone_map.keys():
+            return current
 
-    # handle names like .co.?? (badly)
-    if split[-2] in ["ac", "co"]:
-        join_index = -3
-
-    return ".".join(split[join_index:])
+    return ''
 
 def get_zone_id(cf: 'CF.CloudFlare', zone_name: 'str') -> 'str':
     '''
@@ -298,7 +362,7 @@ def process_records(batch: '_batch') -> 'bool':
             continue
 
         params = record['params']
-        zone_name = get_zone_name(params['name'])
+        zone_name = get_zone_name(cf, params['name'])
         zone_id = get_zone_id(cf, zone_name)
 
         force = batch.force
@@ -343,8 +407,26 @@ def read_json_file(path: 'str') -> 'object':
             print("Error: Invalid format", file=sys.stderr)
         except json.JSONDecodeError:
             print("Error: Failed to parse json", file=sys.stderr)
+        except:
+            print("Error: failed to read file " + file, file=sys.stderr)
 
     return None
+
+def write_json_file(obj, path: 'str') -> 'bool':
+    '''
+    Unmarshall json from file
+    '''
+    with open(path, mode='wt') as file:
+        try:
+            json_str = json.dumps(obj)
+            if file.write(json_str) >= len(json_str):
+                return True
+        except TypeError:
+            print("Error: Failed to create json", file=sys.stderr)
+        except:
+            print("Error: failed to write file " + file, file=sys.stderr)
+
+    return False
 
 def main():
     '''
@@ -398,10 +480,16 @@ def main():
         if not process_records(batch):
             ret = False
 
-    if (ret):
+    return ret
+
+if __name__ == '__main__':
+    CACHE.load()
+    ret = main()
+    CACHE.save()
+
+    if ret:
         sys.exit(EXIT_SUCCESS)
 
     sys.exit(EXIT_FAILURE)
 
-if __name__ == '__main__':
-    main()
+
